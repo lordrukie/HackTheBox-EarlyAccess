8b9826079e78ec47d2b79924e49e871b447b84b8
lordrukie <lordrukie@localhost.localdomain>
2021-09-07 17:08:37 +0900

    Add vallidate.py

---
+++ validate.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import sys
+from re import match
+
+class Key:
+    key = ""
+    magic_value = "XP" # Static (same on API)
+    magic_num = 346 # TODO: Sync with API (api generates magic_num every 30min)
+
+    def __init__(self, key:str, magic_num:int=346):
+        self.key = key
+        if magic_num != 0:
+            self.magic_num = magic_num
+
+    @staticmethod
+    def info() -> str:
+        return f"""
+        # Game-Key validator #
+
+        Can be used to quickly verify a user's game key, when the API is down (again).
+
+        Keys look like the following:
+        AAAAA-BBBBB-CCCC1-DDDDD-1234
+
+        Usage: {sys.argv[0]} <game-key>"""
+
+    def valid_format(self) -> bool:
+        return bool(match(r"^[A-Z0-9]{5}(-[A-Z0-9]{5})(-[A-Z]{4}[0-9])(-[A-Z0-9]{5})(-[0-9]{1,5})$", self.key))
+# Those check for valid key format
+
+
+    def calc_cs(self) -> int:
+        gs = self.key.split('-')[:-1] # remove the last array (1234)
+        return sum([sum(bytearray(g.encode())) for g in gs]) # total all character in bytecode
+
+    def g1_valid(self) -> bool:
+        g1 = self.key.split('-')[0] # Take the first array on key
+        r = [(ord(v)<<i+1)%256^ord(v) for i, v in enumerate(g1[0:3])]
+        # ord = change ascii character to number
+        # x << y = move x to left by y bits (remember binary lesson)
+        # % = remainder of division
+        # i is the iteration count (start from  0) || v is the index value
+        # g1[0:3] = get character from index 0 until character 3 (ABCDE -> ABC)
+        if r != [221, 81, 145]:
+            return False
+        for v in g1[3:]:
+            try:
+                int(v)
+            except:
+                return False
+        return len(set(g1)) == len(g1)
+
+    def g2_valid(self) -> bool:
+        g2 = self.key.split('-')[1]
+        p1 = g2[::2]
+        p2 = g2[1::2]
+        return sum(bytearray(p1.encode())) == sum(bytearray(p2.encode()))
+
+    def g3_valid(self) -> bool:
+        # TODO: Add mechanism to sync magic_num with API
+        g3 = self.key.split('-')[2]
+        if g3[0:2] == self.magic_value:
+            return sum(bytearray(g3.encode())) == self.magic_num
+        else:
+            return False
+
+    def g4_valid(self) -> bool:
+        return [ord(i)^ord(g) for g, i in zip(self.key.split('-')[0], self.key.split('-')[3])] == [12, 4, 20, 117, 0]
+
+    def cs_valid(self) -> bool:
+        cs = int(self.key.split('-')[-1])
+        return self.calc_cs() == cs
+
+    def check(self) -> bool:
+        if not self.valid_format():
+            print('Key format invalid!')
+            return False
+        if not self.g1_valid():
+            print("g1 salah")
+            return False
+        if not self.g2_valid():
+            print("g2 salah")
+            return False
+        if not self.g3_valid():
+            print("g3 salah")
+            return False
+        if not self.g4_valid():
+            print("g4 salah")
+            return False
+        if not self.cs_valid():
+            print('[Critical] Checksum verification failed!')
+            return False
+        return True
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(Key.info())
+        sys.exit(-1)
+    input = sys.argv[1]
+    validator = Key(input)
+    if validator.check():
+        print(f"Entered key is valid!")
+    else:
+        print(f"Entered key is invalid!") 
